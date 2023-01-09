cb5bc6b39adc2f2ec9ae85466f588ea7f9c4925e
Reydet-Eric <eric.reydet@sncf.fr>
2023-01-09 15:33:54 +0100

    ca fonctionne git add .git add .git add .git add .git add .

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -6,5 +6,18 @@ import { Component } from "@angular/core";
   styleUrls: ["./block.component.css"]
 })
 export class BlockComponent {
-  title = "CodeSandbox";
+  title = "angular-binding";
+  user = {
+
+  name : 'Doe',
+
+  firstName : 'John',
+
+  age : 25,
+
+  quote : '',
+
+  photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+
+};
 }
